7721cda19b562a250db7513e7c53bb372e309366
zhongsp <patrick.zhongsp@gmail.com>
2015-03-25 17:35:08 +0800

    update config

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var templateCache = require('gulp-angular-templatecache');
 
 gulp.task('sass', function() {
   gulp.src('./lib/scss/*.scss')
@@ -11,4 +12,12 @@ gulp.task('sass', function() {
 
 gulp.task('watch', function() {
   gulp.watch('./lib/scss/*.scss', ['sass']);
+});
+
+gulp.task('build', function() {
+  gulp.src('lib/**/*.html')
+    .pipe(templateCache('templates.js', {
+      module: 'datepicker'
+    }))
+    .pipe(gulp.dest('build'));
 });
